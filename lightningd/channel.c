1279257fa3c243b3f43a6cf671444e4d4ec7e0b4
Greg Sanders <gsanders87@gmail.com>
2022-09-07 09:26:25 -0400

    failing at channeld creation :)

--- lightningd/channel.c
+++ lightningd/channel.c
@@ -630,12 +630,16 @@ struct channel *new_channel(struct peer *peer, u64 dbid,
 	channel->our_msat = our_msat;
 	channel->msat_to_us_min = msat_to_us_min;
 	channel->msat_to_us_max = msat_to_us_max;
-        channel->last_tx = tal_steal(channel, last_tx);
+	channel->last_tx = tal_steal(channel, last_tx);
 	if (channel->last_tx) {
 		channel->last_tx->chainparams = chainparams;
 	}
-	if (last_sig)
+	channel->last_tx_type = TX_UNKNOWN;
+	if (last_sig) {
 		channel->last_sig = *last_sig;
+	} else if (last_update_sig) {
+		channel->last_update_sig = *last_update_sig;
+	}
 	channel->last_htlc_sigs = tal_steal(channel, last_htlc_sigs);
 	channel->fee_states = dup_fee_states(channel, fee_states);
 	channel->shutdown_scriptpubkey[REMOTE]
