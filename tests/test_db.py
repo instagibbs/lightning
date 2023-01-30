5e1cbced416bdc2723799b63334c41d98089cc3a
Greg Sanders <gsanders87@gmail.com>
2023-01-30 11:23:03 -0500

    just look for psbt magic

--- tests/test_db.py
+++ tests/test_db.py
@@ -212,9 +212,7 @@ def test_last_tx_psbt_upgrade(node_factory, bitcoind):
     last_txs = [x['last_tx'] for x in l2.db_query('SELECT last_tx FROM channels ORDER BY id;')]
 
     # The first tx should be psbt, the second should still be hex (Newer Core version required for better error message)
-    # Hex tx would have given: "TX decode failed invalid base64"
-    with pytest.raises(JSONRPCError, match=r'TX decode failed'):
-        bitcoind.rpc.decodepsbt(base64.b64encode(last_txs[0]).decode('utf-8'))
+    assert last_txs[0][:4] == b'psbt'
 
     bitcoind.rpc.decoderawtransaction(last_txs[1].hex())
 
